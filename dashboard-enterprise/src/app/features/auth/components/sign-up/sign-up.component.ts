77e1ebd601d62f0e8222849d91d26fc740433385
Placide Rigole <foplacide@gmail.com>
2026-02-09 22:17:45 +0100

    feat: Implement initial Angular application setup with PrimeNG integration and a new sign-up component.

--- dashboard-enterprise/src/app/features/auth/components/sign-up/sign-up.component.ts
+++ dashboard-enterprise/src/app/features/auth/components/sign-up/sign-up.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { MODULES_IMPORTS } from '../../../../shared/utils/primeng-imports';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [MODULES_IMPORTS ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
-export class SignUpComponent {
 
+
+
+export class SignUpComponent {
+  public userForm!: FormGroup;
+  constructor(private fb: FormBuilder) {
+      this.userForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      phone: ['', Validators.required, Validators.pattern("^[0-9]*$")],
+      profession: ['', Validators.required],
+      date: ['', Validators.required]
+    })
+  }
 }
---
+++ dashboard-enterprise/src/app/shared/utils/primeng-imports.ts
@@ -0,0 +1,19 @@
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { DatePickerModule } from 'primeng/datepicker';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MessageModule } from 'primeng/message';
+import { CommonModule } from '@angular/common';
+
+export const  MODULES_IMPORTS = [
+  FloatLabelModule,
+  InputTextModule,
+  ButtonModule,
+  TableModule, 
+  DatePickerModule,
+  ReactiveFormsModule,
+  MessageModule,
+  CommonModule
+];
