bd08061c786457aa79456eee06e03abdb55b745e
Placide Rigole <foplacide@gmail.com>
2026-02-11 08:22:34 +0100

    feat: Implement the new dashboard component displaying an employee table with search functionality and header actions.

--- dashboard-enterprise/src/app/features/dashboard/components/dashboard.component.ts
+++ dashboard-enterprise/src/app/features/dashboard/components/dashboard.component.ts
@@ -2,32 +2,33 @@ import { Component, Signal } from '@angular/core';
 import { DashboardStateService } from '../services/dashboard-state.service';
 import { TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
+import { MODULES_IMPORTS } from '../../../shared/utils/primeng-imports';
 
 
 @Component({
   selector: 'app-dashboard',
-  imports: [TableModule, CommonModule],
+  imports: [MODULES_IMPORTS],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-    users!: Signal<any[]>;
-    loading!: Signal<boolean>;
-    error!: Signal<string | null>;
-    search!: Signal<string>;
-    
-    constructor(private dashboardStateService: DashboardStateService) { }
-    
-    ngOnInit(): void {
-        this.users = this.dashboardStateService.filteredEmployees;
-        this.loading = this.dashboardStateService.loading;
-        this.search = this.dashboardStateService.searchTerm;
-        this.error = this.dashboardStateService.error;
-        this.dashboardStateService.loadEmployees();
-    }
+  users!: Signal<any[]>;
+  loading!: Signal<boolean>;
+  error!: Signal<string | null>;
+  search!: Signal<string>;
 
-    onSearchChange(value: string) {
-      console.log(value);
+  constructor(private dashboardStateService: DashboardStateService) { }
+
+  ngOnInit(): void {
+    this.users = this.dashboardStateService.filteredEmployees;
+    this.loading = this.dashboardStateService.loading;
+    this.search = this.dashboardStateService.searchTerm;
+    this.error = this.dashboardStateService.error;
+    this.dashboardStateService.loadEmployees();
+  }
+
+  onSearchChange(value: string) {
+    console.log(value);
     this.dashboardStateService.setSearchTerm(value);
   }
-  }
+}
